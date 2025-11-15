71558b0a0f53d20d6570b1278b770e62e6b41e83
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2025-11-15 23:04:22 +0900

    DummyDataを別クラスで生成するように変更

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_application_multi_checkbox_list/category.dart';
+import 'package:flutter_application_multi_checkbox_list/item.dart';
+
+class DummyData {
+  Category dummyCategory1 = Category(
+    categoryName: 'Category1',
+    itemList: [
+      Item(
+        itemName: 'Name1',
+        checkList: List.filled(3, false),
+      )
+    ],
+  );
+
+  Category dummyCategory2 = Category(
+    categoryName: 'Category2',
+    itemList: [
+      Item(
+        itemName: 'Name2',
+        checkList: List.filled(3, false),
+      ),
+      Item(
+        itemName: 'Name3',
+        checkList: List.filled(3, false),
+      )
+    ],
+  );
+
+  List<Category> getDummyData() {
+    return [
+      dummyCategory1,
+      dummyCategory2,
+    ];
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_multi_checkbox_list/category.dart';
 import 'package:flutter_application_multi_checkbox_list/category_item_tile.dart';
-import 'package:flutter_application_multi_checkbox_list/item.dart';
+import 'package:flutter_application_multi_checkbox_list/data/dummy_data.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,44 +10,45 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-List<bool> dummyDate1 = List.filled(3, false);
-List<bool> dummyDate2 = List.filled(3, false);
-List<bool> dummyDate3 = List.filled(3, false);
+bool isDebug = true;
 
-List<Item> dummyCategory1ItemList = [
-  Item(
-    itemName: 'Name1',
-    checkList: List.filled(3, false),
-  )
-];
-Category dummyCategory1 = Category(
-  categoryName: 'Category1',
-  itemList: dummyCategory1ItemList,
-);
+// Category dummyCategory1 = Category(
+//   categoryName: 'Category1',
+//   itemList: [
+//     Item(
+//       itemName: 'Name1',
+//       checkList: List.filled(3, false),
+//     )
+//   ],
+// );
 
-List<Item> dummyCategory2ItemList = [
-  Item(
-    itemName: 'Name2',
-    checkList: List.filled(3, false),
-  ),
-  Item(
-    itemName: 'Name3',
-    checkList: List.filled(3, false),
-  )
-];
-Category dummyCategory2 = Category(
-  categoryName: 'Category2',
-  itemList: dummyCategory2ItemList,
-);
+// Category dummyCategory2 = Category(
+//   categoryName: 'Category2',
+//   itemList: [
+//     Item(
+//       itemName: 'Name2',
+//       checkList: List.filled(3, false),
+//     ),
+//     Item(
+//       itemName: 'Name3',
+//       checkList: List.filled(3, false),
+//     )
+//   ],
+// );
 
-List<Category> dummyCategoryList = [
-  dummyCategory1,
-  dummyCategory2,
-];
+// List<Category> dummyCategoryList = [
+//   dummyCategory1,
+//   dummyCategory2,
+// ];
 
 class _MyHomePageState extends State<MyHomePage> {
+  late List<Category> categoryList = [];
   @override
   Widget build(BuildContext context) {
+    if (isDebug) {
+      DummyData dummyData = DummyData();
+      categoryList = dummyData.getDummyData();
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -55,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: ListView(
         children: [
-          ...dummyCategoryList.map((category) {
+          ...categoryList.map((category) {
             return ExpansionTile(
               title: Text(category.categoryName),
               children: [
